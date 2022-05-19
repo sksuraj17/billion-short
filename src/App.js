2c46b56d18dcb6d83b5a0f3fb7aefb4b2d02b247
Suraj SK <surajsk2002@gmail.com>
2022-05-19 08:42:02 +0530

    Commit 1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@
 function App() {
 	return (
 		<div className="App">
+			<header>
 			<h1>Billion Short</h1>
+			<form className="search-box">
+				<input type="search" placeholder="Whatever you are looking for is looking for you!"></input>
+			</form>
+			<p>Search Results: 0</p>
+			</header>
+			<div className="results">
+				<div className="result">
+					<h3>Title here</h3>
+					<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+					<a href="#">Read More</a>
+				</div>
+			</div>
 		</div>
 	);
 }
